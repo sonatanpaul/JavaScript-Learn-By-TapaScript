b749c98c7b4cb3abf2b89f0ab0932d88b6130607
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-19 15:57:47 +0600

    leran how to declare variable and how variable reassgin

---
+++ day-02/script.js
@@ -0,0 +1,12 @@
+// declare variable and value assign
+let fruit = "Mango";
+console.log(fruit); // Mango
+
+// Assignment Operator
+("=");
+
+// variable value re-assign
+let name = "Sonatan Paul";
+console.log(name); // Sonatan Paul
+name = "Sonatan";
+console.log(name); // Sonatan
