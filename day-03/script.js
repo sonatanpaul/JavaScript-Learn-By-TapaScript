753c744aa891064844874caf7397c2287ca50ef0
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-19 21:01:27 +0600

    learn assignment operator

--- day-03/script.js
+++ day-03/script.js
@@ -6,3 +6,26 @@ console.log(a + b); // 30
 console.log(b - a); // 10
 console.log(a * b); // 200
 console.log(b / a); // 2
+
+// post increment
+let count = 5;
+console.log(count++); // count = count + 1; // 5
+console.log(count--); // count = count - 1; // 5
+
+// pre increment
+
+let number = 5;
+console.log(++number); // count = count + 1;  // 6
+console.log(--number); // count = count - 1;  // 4
+
+// Assignment Operator
+
+let x = 5;
+console.log(5); // 5
+x = x + 2;
+console.log(x); // 7
+//short hand
+x += 10; // x = x + 7; // 17;
+x -= 2; // x = x - 2; // 15;
+
+
