e65a99d8daae62f013550c3aaa03c1d1a3b547df
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-19 16:08:56 +0600

    learn reassgin variable to another variable

--- day-02/script.js
+++ day-02/script.js
@@ -10,3 +10,14 @@ let name = "Sonatan Paul";
 console.log(name); // Sonatan Paul
 name = "Sonatan";
 console.log(name); // Sonatan
+
+// variable value reassign variable to another variable
+const price1 = 10;
+console.log(price1); // 10
+const price2 = 20;
+console.log(price2); // 20
+
+// re-assign variable to another variable
+price1 = price2;
+console.log(price1); // 20
+console.log(price2); // 20
