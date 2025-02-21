ee265e70871cc9f8d574f7a56b713fb49d679b7c
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-21 21:12:40 +0600

    learn if else and multiple if else

--- day-04/script.js
+++ day-04/script.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+// if else :
+
+let catchingBus = true;
+
+if (catchingBus) {
+  console.log("I will reach home on time");
+} else {
+  console.log("I will be late to reach");
+}
+
+// Example More
+let age = 8;
+
+if (age >= 18) {
+  console.log("You are eligible to Vote");
+} else {
+  console.log("You are not eligible to Vote");
+}
+
+// Ans : You are not eligible to Vote
+
+// Multiple if else
+
+let score = 45;
+
+if (score >= 90) {
+  console.log("Grade - A");
+} else if (score >= 80) {
+  console.log("Grade - B");
+} else if (score >= 70) {
+  console.log("Grade - C");
+} else {
+  console.log("Fail");
+}
