de27836df496fd3113a3592187beb240c4e566f5
sonatanpaul <iamsonatanpaul@gmail.com>
2025-02-24 23:52:55 +0600

    learn for loop

--- Day-05/script.js
+++ Day-05/script.js
@@ -1,2 +1,67 @@
-console.log("hello world");
-console.log("tapascript");
+// for loop
+// for loop structure
+// for (initialization; condition; update) {
+//   //code
+// }
+
+// ==========================================
+
+// Option - 1
+// looping 1 to 5
+// for (let count = 1; count <= 5; count++) {
+//   console.log(count);
+// }
+
+// Option - 2
+// looping 1 to 100;
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// Option - 3
+// findout all even number with for loop
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log("even number is", i);
+//   }
+// }
+
+// Option - 4
+// findout all odd number with for loop
+// for (i = 1; i <= 100; i++) {
+//   if (i % 2 === 1) {
+//     console.log("odd number is", i);
+//   }
+// }
+
+// Option - 5
+// findout all even number and sum all even number
+let sum = 0;
+for (let num = 1; num <= 100; num++) {
+  if (num % 2 === 0) {
+    // sum = sum + num;
+    sum += num;
+  }
+}
+
+console.log(sum);
+
+// Option - 6
+// findout all odd number and sum all odd number
+
+let sum1 = 0;
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 1) {
+    sum1 = sum1 + i;
+  }
+}
+console.log(sum1);
+
+// Option - 7
+// 1 to 100 all number sum
+let sum2 = 0;
+for (let i = 1; i <= 100; i++) {
+  sum2 = sum2 + i;
+}
+
+console.log(sum2);
