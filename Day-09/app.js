572dce1c508a06035862411bf368beb5dab996c5
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-17 21:20:18 +0600

    learn javascript Temporal dead zone

--- Day-09/app.js
+++ Day-09/app.js
@@ -18,3 +18,27 @@ let names = "Tom";
 console.log("name is", names);
 
 */
+
+// Temporal Dead Zone :
+
+// TDZ Means : an area where you can not access a variable  until it is initialized.
+// Reference Error
+
+{
+  // name variable TDZ started here // friend variable TDZ start here
+  //
+  //
+  console.log(name); // reference error
+  //
+  console.log(friend) // reference error
+  //
+  //
+  let name = "Sonatan Paul"; // name variable TDZ end here 
+  console.log(name) // Sonatan Pual // reason TDZ end
+  //
+  //
+  //
+  let friend = 'Anik'  // friend variable TDZ end here
+  console.log(friend) // Anik // reason : TDZ alrady end
+  //
+}
