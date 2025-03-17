f55b123878b816f08d062f64e0c7ff27afeed99e
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-17 21:07:34 +0600

    learn hoisting in let keyword

--- Day-09/app.js
+++ Day-09/app.js
@@ -4,6 +4,14 @@ var names;
 names = "Tom";
 console.log("Names is", names);
 */
+
+/*
+console.log("Name is", names);
+var names;
+names = "Tom";
+console.log("Names is", names);
+*/
+
 /*
 console.log("name is", names);
 let names = "Tom";
