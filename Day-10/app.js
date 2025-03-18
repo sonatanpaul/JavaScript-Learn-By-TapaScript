90f64f7bc24cc34d334cf45d42c9e5d577b8e290
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-18 20:44:56 +0600

    how to declare literal way object and access object

--- Day-10/app.js
+++ Day-10/app.js
@@ -48,6 +48,7 @@ console.log(paulBankAccount(400)); // 600
 console.log(paulBankAccount(1000)); //1600
 */
 
+/*
 function createBankAccount(initialBlance) {
   let blance = initialBlance;
   console.log("Initilazation Blance", blance);
@@ -81,3 +82,24 @@ sonatanAccount.withdraw(50);
 sonatanAccount.withdraw(50);
 sonatanAccount.withdraw(50);
 sonatanAccount.checkBlance();
+
+*/
+
+// Usefulness of Closure
+
+// 1. You can keep the variables private without exposing them.
+// 2. You can stop variable pollution.
+// 3. You can create a function factory.
+// 4. You can keep a variable alive between multiple calls.
+
+/*
+function timer() {
+  let number = 0;
+  document.getElementById("handleClick").addEventListener("click", function () {
+    number++;
+    console.log("Button Clicked", number, "items");
+  });
+}
+
+timer();
+*/
---
+++ Day-11/app.js
@@ -0,0 +1,11 @@
+// Object Learn
+
+// literal Object
+const person = {
+  name: "Sonatan",
+  age: 26,
+};
+
+// access object property
+console.log(person.name); // Sonatan
+console.log(person.age);
