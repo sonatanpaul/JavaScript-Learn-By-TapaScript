f7867e104397f4f117a39e0eea4108664e66c180
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-18 20:58:45 +0600

    learn object update property , modified property, delete property and scpecial key accessable property

--- Day-11/app.js
+++ Day-11/app.js
@@ -14,4 +14,23 @@ console.log(person.age); // 26
 person.isSeniorCitizen = false;
 console.log(person); // total 3 property now
 
+// how to access special object property
+// using bracket notation
+const user = {
+  name: "Paul",
+  age: 26,
+  "Ex-Gf": "Bristi Das",
+};
+console.log(user["Ex-Gf"]);
 
+// how to add modifiy object property
+user.age = 27;
+console.log(user);
+user["Ex-Gf"] = "momo";
+console.log(user["Ex-Gf"]);
+
+// how to delete property
+delete user["Ex-Gf"];
+console.log(user);
+delete user.age;
+console.log(user);
