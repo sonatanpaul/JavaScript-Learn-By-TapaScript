5ef8021deffa82b0542f2f94d08640f2d7ecb7f1
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-26 23:28:35 +0600

    learn try catch throw

--- Day-14/app.js
+++ Day-14/app.js
@@ -39,8 +39,9 @@ function dividedNumbers(a, b) {
   try {
     console.log(a, b);
     if (b === 0) {
-      let err = new Error("Divison by zero is not allowd");
-      throw err;
+      //   let err = new Error("Divison by zero is not allowd");
+      //   throw err;
+      throw new Error("Divison by zero is not allowd");
     }
     const dividedby = a / b;
     console.log(dividedby);
@@ -50,3 +51,45 @@ function dividedNumbers(a, b) {
 }
 
 dividedNumbers(20, 0);
+
+// another example :
+
+/*
+const person = {
+  name: "Paul",
+  address: {
+    city: "Dhaka",
+  },
+};
+
+function getPostalCode(user) {
+  //   console.log(user); get person object
+  try {
+    console.log(person.address.country.postalCode);
+  } catch (error) {
+    console.error("Error Accessing property :", error.message);
+  }
+} //  Error Accessing property : Cannot read properties of undefined (reading 'postalCode')
+
+getPostalCode(person);
+*/
+
+// another example :
+
+function validateAge(age) {
+  //   console.log(age); // 20
+  try {
+    if (isNaN(age)) {
+      throw new Error(
+        `Invalid Number, please input a number, Your input is ${age}`
+      );
+    }
+    console.log(`Your user age is ${age}`); // 20
+  } catch (error) {
+    console.error("Validation Error", error.message);
+  }
+}
+
+validateAge(20);
+validateAge("Tapas");
+validateAge("amar");
