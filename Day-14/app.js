3aac0f271608e4b4f1c052bcbe28274acb85a424
sonatanpaul <iamsonatanpaul@gmail.com>
2025-03-26 20:53:21 +0600

    learn some error type error, range error, sytax error

---
+++ Day-14/app.js
@@ -0,0 +1,11 @@
+// Error :
+
+// console.log(a); // Uncaught ReferenceError: a is not defined
+
+// console.log(x  // Uncaught SyntaxError: missing ) after argument list
+
+const obj = null;
+// console.log(obj.name); // app.js:8 Uncaught TypeError: Cannot read properties of null
+
+// console.log(new Array(-1)); // app.js:10 Uncaught RangeError: Invalid array length
+
