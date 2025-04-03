bde06f4608fa3dc7e34cca575ea6bf2f1fef0176
sonatanpaul <iamsonatanpaul@gmail.com>
2025-04-03 19:42:01 +0600

    learn array method chaining

--- Day-15/app.js
+++ Day-15/app.js
@@ -1190,3 +1190,104 @@ Array.some((element, index, array) =>{
 
   console.log(foundYoungLastIndex); // 4
 }
+
+// Chaining Array Method :
+{
+  let customers = [
+    {
+      id: 1,
+      f_name: "Abby",
+      l_name: "Thomas",
+      gender: "M",
+      married: true,
+      age: 32,
+      expense: 500,
+      purchased: ["Shampoo", "Toys", "Book"],
+    },
+    {
+      id: 2,
+      f_name: "Jerry",
+      l_name: "Tom",
+      gender: "M",
+      married: true,
+      age: 64,
+      expense: 100,
+      purchased: ["Stick", "Blade"],
+    },
+    {
+      id: 3,
+      f_name: "Dianna",
+      l_name: "Cherry",
+      gender: "F",
+      married: true,
+      age: 22,
+      expense: 1500,
+      purchased: ["Lipstik", "Nail Polish", "Bag", "Book"],
+    },
+    {
+      id: 4,
+      f_name: "Dev",
+      l_name: "Currian",
+      gender: "M",
+      married: true,
+      age: 82,
+      expense: 90,
+      purchased: ["Book"],
+    },
+    {
+      id: 5,
+      f_name: "Maria",
+      l_name: "Gomes",
+      gender: "F",
+      married: false,
+      age: 7,
+      expense: 300,
+      purchased: ["Toys"],
+    },
+    {
+      id: 6,
+      f_name: "Maria",
+      l_name: "Gomes",
+      gender: "F",
+      married: false,
+      age: 2,
+      expense: 300,
+      purchased: ["Toys"],
+    },
+  ];
+
+  // Use Case: Get the total amount spent by Married Customers
+
+  // reduce()
+  // map()
+  // filter()
+
+  // Find all the married customers
+  const mariedCustomer = customers.filter((customer) => {
+    return customer.married;
+  });
+  console.log(mariedCustomer);
+  const mariedCustomerExpense = mariedCustomer.map((customer) => {
+    return customer.expense;
+  });
+  console.log(mariedCustomerExpense);
+  const mariedCustomerTotalExpense = mariedCustomerExpense.reduce(
+    (acc, expence) => {
+      return acc + expence;
+    }
+  );
+  console.log(mariedCustomerTotalExpense);
+
+  // Array method Chaining
+  const totalMaridCustomerExpense = customers
+    .filter((customer) => {
+      return customer.married;
+    })
+    .map((cusomer) => {
+      return cusomer.expense;
+    })
+    .reduce((accum, expense) => {
+      return accum + expense;
+    }, 0);
+  console.log(totalMaridCustomerExpense);
+}
