0a8c2190275669b8b8c6bfb0cbeee41ab3f4c9e8
sonatanpaul <iamsonatanpaul@gmail.com>
2025-04-03 19:09:39 +0600

    learn findindex , findlastindex, findlast

--- Day-15/app.js
+++ Day-15/app.js
@@ -994,3 +994,199 @@ Array.some((element, index, array) =>{
 
   console.log(foundYoungCustomer);
 }
+
+// findIndex ()  Method :
+{
+  // find () Method :
+  let customers = [
+    {
+      id: 1,
+      f_name: "Abby",
+      l_name: "Thomas",
+      gender: "M",
+      married: true,
+      age: 32,
+      expense: 500,
+      purchased: ["Shampoo", "Toys", "Book"],
+    },
+    {
+      id: 2,
+      f_name: "Jerry",
+      l_name: "Tom",
+      gender: "M",
+      married: true,
+      age: 64,
+      expense: 100,
+      purchased: ["Stick", "Blade"],
+    },
+    {
+      id: 3,
+      f_name: "Dianna",
+      l_name: "Cherry",
+      gender: "F",
+      married: true,
+      age: 22,
+      expense: 1500,
+      purchased: ["Lipstik", "Nail Polish", "Bag", "Book"],
+    },
+    {
+      id: 4,
+      f_name: "Dev",
+      l_name: "Currian",
+      gender: "M",
+      married: true,
+      age: 82,
+      expense: 90,
+      purchased: ["Book"],
+    },
+    {
+      id: 5,
+      f_name: "Maria",
+      l_name: "Gomes",
+      gender: "F",
+      married: false,
+      age: 7,
+      expense: 300,
+      purchased: ["Toys"],
+    },
+  ];
+  const foundYoungIndex = customers.findIndex((customer) => {
+    return customer.age < 10;
+  });
+
+  console.log(foundYoungIndex); // 4
+}
+
+// findLastIndex ()  Method :
+{
+  // find () Method :
+  let customers = [
+    {
+      id: 1,
+      f_name: "Abby",
+      l_name: "Thomas",
+      gender: "M",
+      married: true,
+      age: 32,
+      expense: 500,
+      purchased: ["Shampoo", "Toys", "Book"],
+    },
+    {
+      id: 2,
+      f_name: "Jerry",
+      l_name: "Tom",
+      gender: "M",
+      married: true,
+      age: 64,
+      expense: 100,
+      purchased: ["Stick", "Blade"],
+    },
+    {
+      id: 3,
+      f_name: "Dianna",
+      l_name: "Cherry",
+      gender: "F",
+      married: true,
+      age: 22,
+      expense: 1500,
+      purchased: ["Lipstik", "Nail Polish", "Bag", "Book"],
+    },
+    {
+      id: 4,
+      f_name: "Dev",
+      l_name: "Currian",
+      gender: "M",
+      married: true,
+      age: 82,
+      expense: 90,
+      purchased: ["Book"],
+    },
+    {
+      id: 5,
+      f_name: "Maria",
+      l_name: "Gomes",
+      gender: "F",
+      married: false,
+      age: 7,
+      expense: 300,
+      purchased: ["Toys"],
+    },
+  ];
+  const foundYoungLastIndex = customers.findLastIndex((customer) => {
+    return customer.age < 10;
+  });
+
+  console.log(foundYoungLastIndex); // 4
+}
+
+// findLast ()  Method :
+{
+  // find () Method :
+  let customers = [
+    {
+      id: 1,
+      f_name: "Abby",
+      l_name: "Thomas",
+      gender: "M",
+      married: true,
+      age: 32,
+      expense: 500,
+      purchased: ["Shampoo", "Toys", "Book"],
+    },
+    {
+      id: 2,
+      f_name: "Jerry",
+      l_name: "Tom",
+      gender: "M",
+      married: true,
+      age: 64,
+      expense: 100,
+      purchased: ["Stick", "Blade"],
+    },
+    {
+      id: 3,
+      f_name: "Dianna",
+      l_name: "Cherry",
+      gender: "F",
+      married: true,
+      age: 22,
+      expense: 1500,
+      purchased: ["Lipstik", "Nail Polish", "Bag", "Book"],
+    },
+    {
+      id: 4,
+      f_name: "Dev",
+      l_name: "Currian",
+      gender: "M",
+      married: true,
+      age: 82,
+      expense: 90,
+      purchased: ["Book"],
+    },
+    {
+      id: 5,
+      f_name: "Maria",
+      l_name: "Gomes",
+      gender: "F",
+      married: false,
+      age: 7,
+      expense: 300,
+      purchased: ["Toys"],
+    },
+    {
+      id: 6,
+      f_name: "Maria",
+      l_name: "Gomes",
+      gender: "F",
+      married: false,
+      age: 2,
+      expense: 300,
+      purchased: ["Toys"],
+    },
+  ];
+  const foundYoungLastIndex = customers.findLast((customer) => {
+    return customer.age < 10;
+  });
+
+  console.log(foundYoungLastIndex); // 4
+}
